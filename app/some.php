167c8a2f0c1ac93b48f4ae47801b70dea4ec9d96
Anton Selin <selin000ant0n@gmail.com>
2017-08-10 18:03:30 +0300

    ref code

--- app/some.php
+++ app/some.php
@@ -1,9 +1,9 @@
 <?php
 
-$fp = fopen("convertBox/convertVersion.xml", "w");
+	$fp = fopen("convertBox/convertVersion.xml", "w");
     
-fwrite($fp, $_POST['string']);
+	fwrite($fp, $_POST['string']);
  
-fclose($fp);
+	fclose($fp);
 
 ?>
